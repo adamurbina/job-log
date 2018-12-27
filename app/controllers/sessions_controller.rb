dd98244d8a54916f5e7e7b2773ef912204926617
Adam <adamurbina@gmail.com>
2018-12-27 17:47:55 -0500

    Added login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to new_user_url
+    else
+      flash[:danger] = "Username or password incorrect"
+      redirect_to login_url
+    end
+
+  end
+
+
 end
