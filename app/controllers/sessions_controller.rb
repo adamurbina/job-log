0a48e5dcd12aedb390d692885c79fee4d8bd072d
Adam <adamurbina@gmail.com>
2018-12-31 08:43:22 -0500

    Added Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,15 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:session][:username])
     if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
+      log_in(@user)
       redirect_to @user
     else
       flash[:danger] = "Username or password incorrect"
       redirect_to login_url
     end
+  end
+
+  def destroy
 
   end
 
